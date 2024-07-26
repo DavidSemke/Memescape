a10dbd25f348ce0c54ede95b8b5120532cc9298a
David <david@semke.ca>
2024-07-26 16:55:11 -0700

    Added more placeholder memes

--- src/data/placeholder/images.ts
+++ src/data/placeholder/images.ts
@@ -18,6 +18,41 @@ export default [
         mime_type: 'image/png',
     },
     {
+        id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144F5',
+        data: fs.readFileSync(`${__dirname}/../images/ackbar.png`),
+        mime_type: 'image/png',
+    },
+    {
+        id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144E5',
+        data: fs.readFileSync(`${__dirname}/../images/ants.png`),
+        mime_type: 'image/png',
+    },
+    {
+        id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144D5',
+        data: fs.readFileSync(`${__dirname}/../images/astronaut.png`),
+        mime_type: 'image/png',
+    },
+    {
+        id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144C5',
+        data: fs.readFileSync(`${__dirname}/../images/cbb.png`),
+        mime_type: 'image/png',
+    },
+    {
+        id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144A5',
+        data: fs.readFileSync(`${__dirname}/../images/dg.png`),
+        mime_type: 'image/png',
+    },
+    {
+        id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C14495',
+        data: fs.readFileSync(`${__dirname}/../images/jim.png`),
+        mime_type: 'image/png',
+    },
+    {
+        id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C14485',
+        data: fs.readFileSync(`${__dirname}/../images/made.png`),
+        mime_type: 'image/png',
+    },
+    {
         id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B4',
         data: fs.readFileSync(`${__dirname}/../images/aint_got_time.png`),
         mime_type: 'image/png',
--- src/data/placeholder/memes.ts
+++ src/data/placeholder/memes.ts
@@ -11,6 +11,98 @@ export default [
         product_image_id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B5',
         create_date: new Date()
     },
+    
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a64420',
+        template_id: 'ackbar',
+        user_id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
+        text: [
+            'when she asks if you think her friend is attractive',
+            'it\'s a trap!'
+        ],
+        private: false,
+        product_image_id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144F5',
+        create_date: new Date()
+    },
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a64421',
+        template_id: 'ants',
+        user_id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
+        text: [
+            'do you want ants?',
+            'because that\'s how you get ants'
+        ],
+        private: false,
+        product_image_id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144E5',
+        create_date: new Date()
+    },
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a64422',
+        template_id: 'astronaut',
+        user_id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
+        text: [
+            'wait, it\'s round?',
+            'always has been',
+            'flat earther',
+            'science'
+        ],
+        private: false,
+        product_image_id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144D5',
+        create_date: new Date()
+    },
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a64423',
+        template_id: 'cbb',
+        user_id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
+        text: [
+            '',
+            'our memes'
+        ],
+        private: false,
+        product_image_id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144C5',
+        create_date: new Date()
+    },
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a64424',
+        template_id: 'dg',
+        user_id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
+        text: [
+            'socialism',
+            'the youth',
+            'capitalism'
+        ],
+        private: false,
+        product_image_id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144A5',
+        create_date: new Date()
+    },
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a64425',
+        template_id: 'jim',
+        user_id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
+        text: [
+            'it\'s ok to like trashy shows and movies',
+            'if it makes you happy then it\'s not a waste of time'
+        ],
+        private: false,
+        product_image_id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C14495',
+        create_date: new Date()
+    },
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a64426',
+        template_id: 'made',
+        user_id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
+        text: [
+            'the internet',
+            'everyone',
+            'i made this.',
+            'you made this?',
+            'i made this.'
+
+        ],
+        private: false,
+        product_image_id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C14485',
+        create_date: new Date()
+    },
     {
         id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
         template_id: 'aint-got-time',
